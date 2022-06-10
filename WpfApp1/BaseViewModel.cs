fbc9b4f9ffd248c79f6c8409f4e7316c9db223e2
Yosef Mahmood <YMahmood@opus2.com>
2022-06-10 22:12:33 +0100

    Add project files.

---
+++ WpfApp1/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp1
+{
+    public partial class BaseViewModel
+    {
+        [RelayCommand]
+        public virtual void TestMethod()
+        {
+            MessageBox.Show("Hello from the Base VM");
+        }
+    }
+}
---
+++ WpfApp1/InheritedViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp1
+{
+    public partial class InheritedViewModel : BaseViewModel
+    {
+
+        [RelayCommand]
+        public override void TestMethod()
+        {
+            MessageBox.Show("Hello from the inherited VM");
+        }
+    }
+}
